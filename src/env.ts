2289d37fc7014f39d17e22a5056d65d8ba242eeb
Lucas Gonçalves <lucasgoncalvesgithub@gmail.com>
2024-02-01 11:12:25 -0300

    Correções gerais

--- src/env.ts
+++ src/env.ts
@@ -1,12 +1,11 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  JWT_KEY: z.string(),
-  JWT_VERIFY_KEY: z.string(),
-  REDIS_PASSWORD: z.string(),
-  // NodeMailer
-  NODEMAILER_MAIL: z.string(),
-  NODEMAILER_PASS: z.string(),
+  JWT_KEY: z.string(), // token-secret
+  JWT_VERIFY_KEY: z.string(), // verify-mail-secret
+  REDIS_PASSWORD: z.string(), // redis-connection-password
+  NODEMAILER_MAIL: z.string(), // nodeMailer-mail
+  NODEMAILER_PASS: z.string(), // nodeMailer-password
 });
 
 export const env = envSchema.parse(process.env);
