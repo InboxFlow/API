1008550e4dc216753c10a0c15c90937405af7974
JohnC0de <joaocarlos.m014@gmail.com>
2024-02-02 12:52:09 -0300

    chore: improve env validator and create .env.example

--- src/env.ts
+++ src/env.ts
@@ -1,11 +1,22 @@
-import { z } from "zod";
+import { z } from 'zod'
 
 const envSchema = z.object({
-  JWT_KEY: z.string(), // token-secret
-  JWT_VERIFY_KEY: z.string(), // verify-mail-secret
-  REDIS_PASSWORD: z.string(), // redis-connection-password
-  NODEMAILER_MAIL: z.string(), // nodeMailer-mail
-  NODEMAILER_PASS: z.string(), // nodeMailer-password
-});
+  //The JWT variables are used to sign and verify the token that is sent to the user's email to verify the account.
+  JWT_KEY: z.string().min(3),
+  JWT_VERIFY_KEY: z.string().min(3),
+  // The REDIS variables are used to connect to the Redis database, which is used to store the user's token and verify the account.
+  REDIS_PASSWORD: z.string().min(3),
+  // The NODEMAILER variables are used to send the email to the user to verify the account.
+  NODEMAILER_MAIL: z.string().email(),
+  NODEMAILER_PASS: z.string().min(3),
+})
 
-export const env = envSchema.parse(process.env);
+const parsedEnv = envSchema.safeParse(process.env)
+if (!parsedEnv.success) throw formatErrorMessage(parsedEnv.error)
+
+export const env = parsedEnv.data
+function formatErrorMessage(error: z.ZodError) {
+  const title = 'Error validating env variables:'
+  const lines = Object.entries(error.flatten().fieldErrors).map(([key, value]) => `-> ${key}: ${value}`)
+  return [title, ...lines].join('\n')
+}
