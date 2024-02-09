a1a5bb42e6a16d12d17bdf0fce231c783aa82152
Lucas Gonçalves <lucasgoncalvesgithub@gmail.com>
2024-02-09 17:50:57 -0300

    feat: correct onError use

--- src/app/use-cases/users/createUser/CreateUserUseCase.ts
+++ src/app/use-cases/users/createUser/CreateUserUseCase.ts
@@ -17,7 +17,7 @@ class CreateUserUseCase {
         .string({ required_error: "Mail is required" })
         .email("Invalid email"),
       password: z
-        .string()
+        .string({ required_error: "Password is required" })
         .min(6, "Password must be at least 6 characters long"),
     });
 
--- src/infra/http/middlewares/onError.ts
+++ src/infra/http/middlewares/onError.ts
@@ -3,11 +3,19 @@ import { HTTP } from "~/shared/helpers";
 
 function onError(error: Error) {
   if (error instanceof ZodError) {
-    return HTTP(400, { message: "Invalid Data", error: error.issues });
+    const issues = error.issues.map((issue: any) => ({
+      expected: issue?.expected || "",
+      received: issue?.received || "",
+      path: issue.path,
+    }));
+
+    return HTTP(400, { message: error.issues[0].message, error: issues });
   }
+
   if (error instanceof Error) {
     return HTTP(500, error);
   }
+
   return HTTP(500, { message: "Internal Server Error" });
 }
 
